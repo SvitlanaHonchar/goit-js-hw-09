bcb84cf437347ba3905040a1dfefa4e636fc33d6
SvitlanaHonchar <snidlty@gmail.com>
2023-01-04 22:25:28 +0200

    task 1.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
 
+let timerID = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStopEl.disabled = true;
+
+function onBtnStartElClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStartEl.disabled = true;
+  btnStopEl.disabled = false;
+}
+
+function onBtnStopElClick() {
+  if (!btnStartEl.disabled) {
+    return;
+  }
+  clearInterval(timerId);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+
+  alert(`Nice color!`);
+}
+
+btnStartEl.addEventListener('click', onBtnStartElClick);
+btnStopEl.addEventListener('click', onBtnStopElClick);
