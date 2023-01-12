52625ccd5a14955d304b0ea6d429f4188b72b01f
SvitlanaHonchar <113723756+SvitlanaHonchar@users.noreply.github.com>
2023-01-12 19:47:13 +0000

    Deploying to gh-pages from @ SvitlanaHonchar/goit-js-hw-09@4d4b45fddd2796c4978dcbaf635c8e0d9d372ccc 🚀

--- 01-color-switcher.ca07b75a.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function d(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.disabled=!0,t.addEventListener("click",(function(){document.body.style.backgroundColor=d(),timerId=setInterval((()=>{document.body.style.backgroundColor=d()}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){t.disabled&&(clearInterval(timerId),t.disabled=!1,e.disabled=!0,alert("Nice color!"))}));
-//# sourceMappingURL=01-color-switcher.ca07b75a.js.map
